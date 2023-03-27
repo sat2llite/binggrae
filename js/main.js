774a2f255318debf2b4273f4f4ec31bf1337d18f
sat2llite <63222786+sat2llite@users.noreply.github.com>
2023-03-27 09:25:04 +0900

    빙그레 메인1 서브2
    내비게이션x

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// const header = document.querySelector("header");
+// const gnb = document.querySelector("#gnb")
+
+// gnb.onmouseover = () => {
+//   header.classList.add("active");
+// }
+// gnb.onmouseout = () => {
+//   header.classList.remove("active");
+// }
